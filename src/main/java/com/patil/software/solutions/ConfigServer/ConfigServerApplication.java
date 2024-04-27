8aceb4fc8a97415d38dd92bf1193a731a7c6eaae
ShridharMP <mpshridhar2016@gmail.com>
2024-04-27 18:33:19 +0530

    Commit in config Server

---
+++ src/main/java/com/patil/software/solutions/ConfigServer/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.patil.software.solutions.ConfigServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
