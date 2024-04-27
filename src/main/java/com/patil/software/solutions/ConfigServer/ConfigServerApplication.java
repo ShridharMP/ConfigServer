cc68c70c7020c831424385261d314fe1993e0c51
ShridharMP <mpshridhar2016@gmail.com>
2024-04-28 00:47:50 +0530

    latest commits

--- src/main/java/com/patil/software/solutions/ConfigServer/ConfigServerApplication.java
+++ src/main/java/com/patil/software/solutions/ConfigServer/ConfigServerApplication.java
@@ -2,8 +2,10 @@ package com.patil.software.solutions.ConfigServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
